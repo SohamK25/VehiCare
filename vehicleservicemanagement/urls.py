d667f30c73bc7524f951bfa67c792d8aecd0cc43
Soham Kulkarni <ksoham2002@gmail.com>
2024-04-21 22:35:07 +0530

    initial and updates

--- vehicleservicemanagement/urls.py
+++ vehicleservicemanagement/urls.py
@@ -4,7 +4,7 @@ vehicle
 from django.contrib import admin
 from django.urls import path
 from vehicle import views
-from django.contrib.auth import LoginView,LogoutView
+from django.contrib.auth.views import LoginView,LogoutView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -93,8 +93,16 @@ urlpatterns = [
     path('afterlogin/', views.afterlogin_view,name='afterlogin'),
     
     path('logout/', views.logout_view,name='logout'),
+    
+
 
     path('aboutus', views.aboutus_view),
     path('services', views.services_view),
     path('contactus', views.contactus_view),
+    
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    
 ]
